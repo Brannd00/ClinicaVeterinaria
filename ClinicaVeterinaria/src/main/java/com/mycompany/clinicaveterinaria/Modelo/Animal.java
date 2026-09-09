92e8572515bed41d29b68f6f1d1e88e89ae7d2c8
Brandon <bvillalobosp1@unicartagena.edu.co>
2026-09-09 16:34:59 -0500

    arreglo

--- ClinicaVeterinaria/src/main/java/com/mycompany/clinicaveterinaria/Modelo/Animal.java
+++ ClinicaVeterinaria/src/main/java/com/mycompany/clinicaveterinaria/Modelo/Animal.java
@@ -4,54 +4,10 @@
  */
 package com.mycompany.clinicaveterinaria.Modelo;
 
-public abstract class Animal {
-
-    private String nombre;
-    protected String especie;
-    int edad;              
-    public String colorPelaje;
-
+/**
+ *
+ * @author brann
+ */
+public class Animal {
     
-    public Animal(String nombre, String especie, int edad, String colorPelaje) {
-        this.nombre = nombre;
-        this.especie = especie;
-        this.edad = edad;
-        this.colorPelaje = colorPelaje;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEspecie() {
-        return especie;
-    }
-
-    public void setEspecie(String especie) {
-        this.especie = especie;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public String getColorPelaje() {
-        return colorPelaje;
-    }
-
-    public void setColorPelaje(String colorPelaje) {
-        this.colorPelaje = colorPelaje;
-    }
-
-    public String describir() {
-        return nombre + " (" + especie + "), " + edad + " años, pelaje " + colorPelaje;
-    }
 }
